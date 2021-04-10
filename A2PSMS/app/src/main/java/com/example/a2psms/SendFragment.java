e8e1c21143528681b2cd23a7c82ba776572a8521
gwit13 <gwit0513@gmail.com>
2021-04-10 19:22:38 -0400

    Implement TabLayout and ViewPager, Fragment visible

--- A2PSMS/app/src/main/java/com/example/a2psms/SendFragment.java
+++ A2PSMS/app/src/main/java/com/example/a2psms/SendFragment.java
@@ -8,14 +8,33 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class SendFragment extends Fragment {
-    public SendFragment() {}
+    int count;
+    ViewPager2 mViewPager2;
+
+    public static Fragment newInstance(ViewPager2 mViewPager2, int position) {
+        SendFragment fragment = new SendFragment();
+        fragment.mViewPager2 = mViewPager2;
+        fragment.count = position;
+        return fragment;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_send, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TabLayout tabLayout = getActivity().findViewById(R.id.tabframe);
+        new TabLayoutMediator(tabLayout, mViewPager2, ((tab, position) -> tab.setText("Placeholder"))).attach();
     }
 }
