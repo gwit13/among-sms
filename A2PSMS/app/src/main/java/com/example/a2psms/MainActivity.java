79794a4c6c33612baecf6743508a287dc3b3c694
gwit13 <gwit0513@gmail.com>
2021-04-10 12:45:46 -0400

    Instantiate SMSReceiver

--- A2PSMS/app/src/main/java/com/example/a2psms/MainActivity.java
+++ A2PSMS/app/src/main/java/com/example/a2psms/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity{
+    SMSReceiver sms;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //instantiations
+        sms = new SMSReceiver();
     }
 }
