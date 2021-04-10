ccb0e91c219d40ade1ffb05f1ebf86c59f47e3fa
Nicholas Rising <nicholas.rising@gmail.com>
2021-04-10 18:21:35 -0400

    SMSReceiver

--- A2PSMS/app/src/main/java/com/example/a2psms/MainActivity.java
+++ A2PSMS/app/src/main/java/com/example/a2psms/MainActivity.java
@@ -4,13 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity{
-    SMSReceiver sms;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //instantiations
-        sms = new SMSReceiver();
     }
 }
